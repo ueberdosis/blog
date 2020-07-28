13a4babe7f3ec0884a93e2fe29683f5aeef3f7ba
Hans Pagel <hans.pagel@ueber.io>
2020-07-28 13:40:25 +0200

    add open graph data

--- src/main.js
+++ src/main.js
@@ -4,6 +4,36 @@
 import DefaultLayout from '~/layouts/Default.vue'
 
 export default function (Vue, { router, head, isClient }) {
+  head.meta.push({
+    name: 'description',
+    content: 'A digital product design agency, building own products & working for clients.',
+  })
+
+  head.meta.push({
+    property: 'og:title',
+    content: 'überdosis',
+  })
+
+  head.meta.push({
+    property: 'og:url',
+    content: 'https://blog.ueber.io',
+  })
+
+  // head.meta.push({
+  //   property: 'og:image',
+  //   content: 'https://blog.ueber.io/opengraph.png',
+  // })
+
+  head.meta.push({
+    property: 'og:description',
+    content: 'A digital product design agency, building own products & working for clients.',
+  })
+
+  head.meta.push({
+    name: 'twitter:card',
+    content: 'summary_large_image',
+  })
+
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 }
